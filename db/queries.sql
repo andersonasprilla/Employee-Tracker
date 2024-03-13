206c481d0730fc95a8f3b31eec2d04de86ae387e
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-03-13 19:07:08 -0400

    update queries

--- db/queries.sql
+++ db/queries.sql
@@ -1,13 +1,24 @@
 USE employee_db;
 
 SELECT * FROM department;
-SELECT * FROM role;
-SELECT id, title, salary FROM role;
 
-
-
-SELECT role.id AS id, title, department.name AS department, salary
+SELECT
+    role.id as id,
+    title,
+    name as department,
+    salary
 FROM role
-LEFT JOIN department ON role.department_id = department.id;
+    LEFT JOIN department ON role.department_id = department.id;
 
-UPDATE employee SET manager_id = 1 WHERE id = 2;
+SELECT
+    employee.id as id,
+    employee.first_name,
+    employee.last_name,
+    title,
+    name as department,
+    salary,
+    CONCAT(managerTable.first_name, ' ', managerTable.last_name) as manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id 
+    LEFT JOIN department ON role.department_id = department.id 
+    LEFT JOIN employee as managerTable ON employee.manager_id = managerTable.id 
