962dfbff9c9de05354e55eac5967cde5200a2a11
Anderson Asprilla <anderson.asprilla@gmail.com>
2024-03-13 18:51:10 -0400

    fix queries bug

--- db/queries.sql
+++ db/queries.sql
@@ -1,6 +1,13 @@
 USE employee_db;
 
 SELECT * FROM department;
+SELECT * FROM role;
+SELECT id, title, salary FROM role;
 
-SELECT roles.id as id , title, name as department, salary FROM roles
-LEFT JOIN department ON role.department_id=department.id;
+
+
+SELECT role.id AS id, title, department.name AS department, salary
+FROM role
+LEFT JOIN department ON role.department_id = department.id;
+
+UPDATE employee SET manager_id = 1 WHERE id = 2;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,27 @@
-INSERT INTO
-    department (name)
+INSERT INTO department (name)
 VALUES ('Engineering'),
-    ('Finance'),
-    ('Legal'),
-    ('Sales');
+       ('Finance'),
+       ('Legal'),
+       ('Sales');
 
-    INSERT INTO
-    roles (title)
-VALUES ('Sales Lead',4),
-    ('Salesperson',4),
-    ('Lead Engineer',1),
-    ('Account Manager',2),
-    ('Accountant',2),
-    ('legal Team Lead',3),
-    ('Lawyer',3);
+INSERT INTO role (title, department_id, salary)
+VALUES ('Sales Lead', 4, 100000.00),
+       ('Salesperson', 4, 80000.00),
+       ('Lead Engineer', 1, 150000.00),
+       ('Software Engineer', 1, 120000.00),
+       ('Account Manager', 2, 160000.00),
+       ('Accountant', 2, 125000.00),
+       ('Legal Team Lead', 3, 250000.00),
+       ('Lawyer', 3, 190000.00);
 
-     INSERT INTO
-    employee (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES ('John', 'Doe', 4),
-    ('Mike', 'Chan', 4),
-    ('Ashley', 'Rodriguez', 1),
-    ('Kevin', 'Tupik', 1),
-    ('Kunal', 'Singh', 2),
-    ('Malia', 'Browm', 2),
-    ('Sarah', 'Lourd', 3),
-    ('Tom', 'Allen', 3);
+       ('Mike', 'Chan', 4),
+       ('Ashley', 'Rodriguez', 1),
+       ('Kevin', 'Tupik', 1),
+       ('Kunal', 'Singh', 2),
+       ('Malia', 'Brown', 2),
+       ('Sarah', 'Lourd', 3),
+       ('Tom', 'Allen', 3);
+
+
